d98eb62926ac386f909dd6144e7213bfce5c30f7
Hwangwonuk <86584999+Hwangwonuk@users.noreply.github.com>
2022-03-08 13:41:46 +0900

    Hello JPA - 애플리케이션 개발

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -9,6 +9,12 @@
  */
 package hellojpa;
 
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
 /**
  * create on 2022/03/08. create by IntelliJ IDEA.
  *
@@ -23,7 +29,37 @@ package hellojpa;
 public class JpaMain {
 
   public static void main(String[] args) {
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
 
+    EntityManager em = emf.createEntityManager();
+
+    EntityTransaction tx = em.getTransaction();
+    tx.begin();
+
+    try {
+//      Member member = new Member();
+//      member.setId(2L);
+//      member.setName("HelloA");
+//      em.persist(member);
+
+//      Member findMember = em.find(Member.class, 1L);
+      List<Member> result = em.createQuery("select  m from Member as m", Member.class)
+          .setFirstResult(5)
+          .setMaxResults(8)
+          .getResultList();
+
+      for (Member member : result) {
+        System.out.println("member.getName() = " + member.getName());
+      }
+      
+      tx.commit();
+    } catch (Exception e) {
+      tx.rollback();
+    } finally {
+      em.close();
+    }
+
+    emf.close();
   }
 
 }
