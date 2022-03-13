fecff8293145fb5693439f8cbc39996bf17d34d9
Hwangwonuk <86584999+Hwangwonuk@users.noreply.github.com>
2022-03-14 01:05:19 +0900

    데이터베이스 스키마 자동 생성

---
+++ ex1-hello-jpa/src/main/main/java/hellojpa/Member.java
@@ -0,0 +1,61 @@
+/*
+ * Created by Wonuk Hwang on 2022/03/08
+ * As part of Bigin
+ *
+ * Copyright (C) Bigin (https://bigin.io/main) - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by infra Team <wonuk_hwang@bigin.io>, 2022/03/08
+ */
+package hellojpa;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * create on 2022/03/08. create by IntelliJ IDEA.
+ *
+ * <p> </p>
+ * <p> {@link } and {@link } </p> *
+ *
+ * @author wonukHwang
+ * @version 1.0
+ * @see
+ * @since (ex : 5 + 5)
+ */
+@Entity
+public class Member {
+
+  @Id
+  private Long id;
+
+//  @Column(name = "username") 컬럼명이 다를 경우
+  @Column(unique = true, length = 10)
+  private String name;
+
+  public Member() {
+  }
+
+  public Member(Long id, String name) {
+    this.id = id;
+    this.name = name;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
