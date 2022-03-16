a4634b92930c9235dd9db77e9c0446cc2a872adc
Hwangwonuk <86584999+Hwangwonuk@users.noreply.github.com>
2022-03-16 20:59:34 +0900

    기본 키 매핑

--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -16,9 +16,13 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+import javax.persistence.TableGenerator;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.persistence.Transient;
@@ -35,40 +39,23 @@ import javax.persistence.Transient;
  * @since (ex : 5 + 5)
  */
 @Entity
-
+@SequenceGenerator(
+    name = "MEMBER_SEQ_GENERATOR",
+    sequenceName = "MEMBER_SEQ", // 매핑할 데이터베이스 시퀀스 이동
+    initialValue = 1, allocationSize = 50)
 public class Member {
 
   @Id
+  @GeneratedValue(strategy = GenerationType.TABLE,
+          generator = "MEMBER_SEQ_GENERATOR")
   private Long id;
 
   @Column(name = "name", nullable = false)
   private String username;
 
-  private Integer age;
-
-  @Enumerated(EnumType.STRING)
-  private RoleType roleType;
-
-  @Temporal(TemporalType.TIMESTAMP)
-  private Date createdDate;
-
-  @Temporal(TemporalType.TIMESTAMP)
-  private Date lastModifiedDate;
-
-  private LocalDate testLocalDate;
-  private LocalDateTime testLocalDateTime;
-
-  @Lob
-  private String description;
-
-  @Transient
-  private int temp;
-
   public Member() {
 
   }
-
-//Getter, Setter...
 
   public Long getId() {
     return id;
@@ -84,53 +71,5 @@ public class Member {
 
   public void setUsername(String username) {
     this.username = username;
-  }
-
-  public Integer getAge() {
-    return age;
-  }
-
-  public void setAge(Integer age) {
-    this.age = age;
-  }
-
-  public RoleType getRoleType() {
-    return roleType;
-  }
-
-  public void setRoleType(RoleType roleType) {
-    this.roleType = roleType;
-  }
-
-  public Date getCreatedDate() {
-    return createdDate;
-  }
-
-  public void setCreatedDate(Date createdDate) {
-    this.createdDate = createdDate;
-  }
-
-  public Date getLastModifiedDate() {
-    return lastModifiedDate;
-  }
-
-  public void setLastModifiedDate(Date lastModifiedDate) {
-    this.lastModifiedDate = lastModifiedDate;
-  }
-
-  public String getDescription() {
-    return description;
-  }
-
-  public void setDescription(String description) {
-    this.description = description;
-  }
-
-  public int getTemp() {
-    return temp;
-  }
-
-  public void setTemp(int temp) {
-    this.temp = temp;
   }
 }
