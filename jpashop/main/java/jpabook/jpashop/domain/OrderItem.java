dc03718305f18cce6d6e38f0dc11c3de8dbeafa5
Hwangwonuk <86584999+Hwangwonuk@users.noreply.github.com>
2022-03-18 20:55:44 +0900

    실전 예제 1 - 요구사항 분석과 기본 매핑

---
+++ jpashop/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -0,0 +1,83 @@
+/*
+ * Created by Wonuk Hwang on 2022/03/18
+ * As part of Bigin
+ *
+ * Copyright (C) Bigin (https://bigin.io/main) - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by infra Team <wonuk_hwang@bigin.io>, 2022/03/18
+ */
+package jpabook.jpashop.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * create on 2022/03/18. create by IntelliJ IDEA.
+ *
+ * <p> </p>
+ * <p> {@link } and {@link } </p> *
+ *
+ * @author wonukHwang
+ * @version 1.0
+ * @see
+ * @since (ex : 5 + 5)
+ */
+@Entity
+public class OrderItem {
+
+  @Id @GeneratedValue
+  @Column(name = "ORDER_ITEM_ID")
+  private Long id;
+
+  @Column(name = "ORDER_ID")
+  private Long orderId;
+
+  @Column(name = "ITEM_ID")
+  private Long itemId;
+
+  private int orderPrice;
+  private int count;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public Long getOrderId() {
+    return orderId;
+  }
+
+  public void setOrderId(Long orderId) {
+    this.orderId = orderId;
+  }
+
+  public Long getItemId() {
+    return itemId;
+  }
+
+  public void setItemId(Long itemId) {
+    this.itemId = itemId;
+  }
+
+  public int getOrderPrice() {
+    return orderPrice;
+  }
+
+  public void setOrderPrice(int orderPrice) {
+    this.orderPrice = orderPrice;
+  }
+
+  public int getCount() {
+    return count;
+  }
+
+  public void setCount(int count) {
+    this.count = count;
+  }
+}
